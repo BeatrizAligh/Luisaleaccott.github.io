fd8ef226505aed557a10cfa78b4c998d2cfb824e
BeatrizAligh <100534216+BeatrizAligh@users.noreply.github.com>
2023-06-09 21:07:45 -0500

    Actualizacion1

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,4 +1,19 @@
-﻿function saludo() {
-    alert("Hola Mundo");
+﻿document.addEventListener("DOMContentLoaded", function () {
+    function reveal() {
+        var reveals = document.querySelectorAll(".reveal");
 
-}
+        reveals.forEach((reveal) => {
+            var windowHeigh = window.innerHeight;
+            var elementsTop = reveal.getBoundingClientRect().top;
+            var elementsVisible = 100;
+
+            if (elementsTop < windowHeigh - elementsVisible) {
+                reveal.classList.add("active");
+            } else {
+                reveal.classList.remove("active");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", reveal);
+});
